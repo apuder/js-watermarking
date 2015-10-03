a12642c564261f77b3ee18d31dc65dc6d6661141
Jason Burmark <mr.burmark@gmail.com>
2015-10-03 03:42:10 -0700

    Add esmorph to add code into functions easily

--- watermark.js
+++ watermark.js
@@ -1,13 +1,42 @@
 /// <reference path="typings/esprima/esprima.d.ts" />
 /// <reference path="typings/escodegen/escodegen.d.ts" />
+/// <reference path="typings/esmorph/esmorph.d.ts" />
 var esprima = require("esprima");
 var escodegen = require("escodegen");
-var original_code_string = 'var answer = 42';
+var esmorph = require("esmorph");
+var original_code_string = 'function question() {\n return 7 * 9 }';
 var code_ast = esprima.parse(original_code_string);
 var generated_code = escodegen.generate(code_ast);
+var tracerEntrance = esmorph.Tracer.FunctionEntrance(function (fn) {
+    var sig = 'console.log("Call ' + fn.name;
+    sig += ' at line ' + fn.loc.start.line;
+    sig += ' in range [' + fn.range[0] + ',' + fn.range[1] + ']");';
+    return sig;
+});
+var tracerExit = esmorph.Tracer.FunctionExit(function (fn) {
+    var sig = 'console.log("Exit ' + fn.name;
+    sig += ' at line ' + fn.loc.end.line;
+    sig += ' in range [' + fn.range[0] + ',' + fn.range[1] + ']");';
+    return sig;
+});
+var morphed_code_entry = esmorph.modify(original_code_string, tracerEntrance);
+var morphed_code_exit = esmorph.modify(original_code_string, tracerExit);
+var morphed_code_both = esmorph.modify(morphed_code_entry, tracerExit);
 // print original code
 console.log(JSON.stringify(original_code_string, null, 4));
+console.log();
 // print generated abstract syntax tree
 console.log(JSON.stringify(code_ast, null, 4));
+console.log();
 // print regenerated code
 console.log(JSON.stringify(generated_code, null, 4));
+console.log();
+// print code with extra statements at beginning
+console.log(JSON.stringify(morphed_code_entry, null, 4));
+console.log();
+// print code with extra statements at end
+console.log(JSON.stringify(morphed_code_exit, null, 4));
+console.log();
+// print code with extra statements at beginning and end
+console.log(JSON.stringify(morphed_code_both, null, 4));
+console.log();
--- watermark.ts
+++ watermark.ts
@@ -1,19 +1,54 @@
 
 /// <reference path="typings/esprima/esprima.d.ts" />
 /// <reference path="typings/escodegen/escodegen.d.ts" />
+/// <reference path="typings/esmorph/esmorph.d.ts" />
 
 import esprima = require("esprima");
 import escodegen = require("escodegen");
+import esmorph = require("esmorph");
 
-var original_code_string : string = 'var answer = 42';
+
+var original_code_string : string = 'function question() {\n return 7 * 9 }';
 
 var code_ast : ESTree.Program = esprima.parse(original_code_string);
 
 var generated_code : string = escodegen.generate(code_ast);
 
+var tracerEntrance = esmorph.Tracer.FunctionEntrance(function (fn) {
+	var sig = 'console.log("Call ' + fn.name;
+	sig += ' at line ' + fn.loc.start.line;
+	sig += ' in range [' + fn.range[0] + ',' + fn.range[1] + ']");';
+	return sig;
+});
+
+var tracerExit = esmorph.Tracer.FunctionExit(function (fn) {
+	var sig = 'console.log("Exit ' + fn.name;
+	sig += ' at line ' + fn.loc.end.line;
+	sig += ' in range [' + fn.range[0] + ',' + fn.range[1] + ']");';
+	return sig;
+});
+
+var morphed_code_entry: string = esmorph.modify(original_code_string, tracerEntrance);
+
+var morphed_code_exit: string = esmorph.modify(original_code_string, tracerExit);
+
+var morphed_code_both: string = esmorph.modify(morphed_code_entry, tracerExit);
+
 // print original code
 console.log(JSON.stringify(original_code_string, null, 4));
+console.log();
 // print generated abstract syntax tree
 console.log(JSON.stringify(code_ast, null, 4));
+console.log();
 // print regenerated code
 console.log(JSON.stringify(generated_code, null, 4));
+console.log();
+// print code with extra statements at beginning
+console.log(JSON.stringify(morphed_code_entry, null, 4));
+console.log();
+// print code with extra statements at end
+console.log(JSON.stringify(morphed_code_exit, null, 4));
+console.log();
+// print code with extra statements at beginning and end
+console.log(JSON.stringify(morphed_code_both, null, 4));
+console.log();
