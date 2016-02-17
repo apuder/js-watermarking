30e404d79db1ad2fce4e14a46b8ee81728f3cc07
Jason Burmark <mr.burmark@gmail.com>
2016-02-17 03:15:19 -0800

    added pdfjs viewer page in chrome app

--- app/background.js
+++ app/background.js
@@ -1,16 +1,29 @@
 
-blacklistedIds = ["none"];
+// blacklistedIds = ["none"];
 
-chrome.runtime.onMessageExternal.addListener(
-  function(request, sender, sendResponse) {
-    if (sender.id in blacklistedIds) {
-      sendResponse({"result":"sorry, could not process your message"});
-      return;  // don't allow this extension access
-    } else if (request.BAclick) {
-      sendResponse({"result":"Ok, got your message"});
-      chrome.tabs.executeScript(null, {file: "savefile.js"});
-    } else {
-      sendResponse({"result":"Ops, I don't understand this message"});
-    }
+// chrome.runtime.onMessageExternal.addListener(
+//   function(request, sender, sendResponse) {
+//     if (sender.id in blacklistedIds) {
+//       sendResponse({"result":"sorry, could not process your message"});
+//       return;  // don't allow this extension access
+//     } else if (request.BAclick) {
+//       sendResponse({"result":"Ok, got your message"});
+//       // chrome.tabs.executeScript(null, {file: "savefile.js"});
+//       chrome.tabs.create({
+//         "url": chrome.extension.getURL("web/web/viewer.html")
+//       }); 
+//     } else {
+//       sendResponse({"result":"Ops, I don't understand this message"});
+//     }
+//   });
+
+
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('web/web/viewer.html', {
+    // id: "host",
+    // innerBounds: {
+    //   width: 800,
+    //   height: 600,
+    // }
   });
-
+});
